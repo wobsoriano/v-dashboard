6599d1ca9328dfcb1b37b38aaab243c9431e29a0
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-09-21 19:53:42 +0800

    Opt-in to purgeLayersByDefault in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
   },
   purge: {
     enabled: process.env.NODE_ENV === 'production',
