ac450784724f52f369d4598e22ccf0d766dab00e
Robert Soriano <sorianorobertc@gmail.com>
2021-06-23 10:10:43 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ whitelist = ["gray", "red", "orange", "yellow", "green", "teal", "blue", "purple
   (result, color) => result.push(`text-${color}-600`, `bg-${color}-600`, `bg-${color}-500`) && result, [])
 
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    options: {
+      whitelist,
+    }
+  },
   theme: {
     extend: {
       fontFamily: {
