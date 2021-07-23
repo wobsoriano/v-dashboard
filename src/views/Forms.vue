73b88ec4d779593acfe09e9940b9808ac85cd0bf
khatabwedaa <khatabwedaa@gmail.com>
2021-07-23 09:34:45 +0200

    Fix forms inputs

--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <h3 class="text-gray-700 text-3xl font-semibold">Forms</h3>
+    <h3 class="text-3xl font-semibold text-gray-700">Forms</h3>
 
     <div class="mt-4">
       <h4 class="text-gray-600">Model Form</h4>
 
       <div class="mt-4">
         <div
-          class="max-w-sm w-full bg-white shadow-md rounded-md overflow-hidden border"
+          class="w-full max-w-sm overflow-hidden bg-white border rounded-md shadow-md"
         >
           <form>
             <div
-              class="flex justify-between items-center px-5 py-3 text-gray-700 border-b"
+              class="flex items-center justify-between px-5 py-3 text-gray-700 border-b"
             >
               <h3 class="text-sm">Add Category</h3>
               <button>
                 <svg
-                  class="h-4 w-4"
+                  class="w-4 h-4"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -31,15 +31,15 @@
               </button>
             </div>
 
-            <div class="px-5 py-6 bg-gray-200 text-gray-700 border-b">
+            <div class="px-5 py-6 text-gray-700 bg-gray-200 border-b">
               <label class="text-xs">Name</label>
 
-              <div class="mt-2 relative rounded-md shadow-sm">
+              <div class="relative mt-2 rounded-md shadow-sm">
                 <span
-                  class="absolute inset-y-0 left-0 pl-3 flex items-center text-gray-600"
+                  class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600"
                 >
                   <svg
-                    class="h-6 w-6"
+                    class="w-6 h-6"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -55,19 +55,19 @@
 
                 <input
                   type="text"
-                  class="w-full px-12 py-2 appearance-none rounded-md focus:border-indigo-600"
+                  class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
                 />
               </div>
             </div>
 
-            <div class="flex justify-between items-center px-5 py-3">
+            <div class="flex items-center justify-between px-5 py-3">
               <button
-                class="px-3 py-1 text-gray-700 text-sm rounded-md bg-gray-200 hover:bg-gray-300 focus:outline-none"
+                class="px-3 py-1 text-sm text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 focus:outline-none"
               >
                 Cancel
               </button>
               <button
-                class="px-3 py-1 bg-indigo-600 text-white rounded-md text-sm hover:bg-indigo-500 focus:outline-none"
+                class="px-3 py-1 text-sm text-white bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none"
               >
                 Save
               </button>
@@ -82,16 +82,16 @@
 
       <div class="mt-4">
         <div class="p-6 bg-white rounded-md shadow-md">
-          <h2 class="text-lg text-gray-700 font-semibold capitalize">
+          <h2 class="text-lg font-semibold text-gray-700 capitalize">
             Account settings
           </h2>
 
           <form @submit.prevent="register">
-            <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-4">
+            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
               <div>
                 <label class="text-gray-700" for="username">Username</label>
                 <input
-                  class="w-full mt-2 rounded-md focus:border-indigo-600"
+                  class="w-full mt-2 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
                   type="text"
                   v-model="user.username"
                 />
@@ -102,7 +102,7 @@
                   >Email Address</label
                 >
                 <input
-                  class="w-full mt-2 rounded-md focus:border-indigo-600"
+                  class="w-full mt-2 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
                   type="email"
                   v-model="user.email"
                 />
@@ -111,7 +111,7 @@
               <div>
                 <label class="text-gray-700" for="password">Password</label>
                 <input
-                  class="w-full mt-2 rounded-md focus:border-indigo-600"
+                  class="w-full mt-2 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
                   type="password"
                   v-model="user.password"
                 />
@@ -122,7 +122,7 @@
                   >Password Confirmation</label
                 >
                 <input
-                  class="w-full mt-2 rounded-md focus:border-indigo-600"
+                  class="w-full mt-2 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
                   type="password"
                   v-model="user.confirm"
                 />
@@ -131,7 +131,7 @@
 
             <div class="flex justify-end mt-4">
               <button
-                class="px-4 py-2 bg-gray-800 text-gray-200 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+                class="px-4 py-2 text-gray-200 bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
               >
                 Save
               </button>
