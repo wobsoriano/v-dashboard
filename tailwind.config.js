94bb45cb8c86a675b72d74d6c5f693382a6c022e
Robert Soriano <sorianorobertc@gmail.com>
2022-02-25 09:37:59 -0800

    bump deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
-whitelist = ["gray", "red", "orange", "yellow", "green", "teal", "blue", "purple", "pink"].reduce(
-  (result, color) => result.push(`text-${color}-600`, `bg-${color}-600`, `bg-${color}-500`) && result, [])
-
 module.exports = {
   mode: 'jit',
-  purge: {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    options: {
-      whitelist,
-    }
-  },
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
@@ -21,6 +13,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio')
+    require('@tailwindcss/aspect-ratio'),
   ],
 };
